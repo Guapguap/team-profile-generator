c18be29f0808d573690a41b961e09f04da208d34
alberto londono <albertolondono.londono5@gmail.com>
2022-06-11 22:21:27 -0500

    Create test files. Engineer test success

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,5 +1,8 @@
 const Employee = require('../lib/Employee');
 
+// this tests the parent class 
+
+// creates the new employee object 
 test('Creates new employee', () => {
     const employee = new Employee('Alberto', 80, 'someEmail@gmail.com');
 
@@ -9,3 +12,12 @@ test('Creates new employee', () => {
     
 })
 
+// gets all the values from the methods 
+test('Checks all methods for Employee class', () => {
+    const employee = new Employee('Alberto', 80, 'someEmail@gmail.com');
+
+    expect(employee.getName()).toBe(employee.name);
+    expect(employee.getId()).toBe(employee.id);
+    expect(employee.getEmail()).toBe(employee.email);
+    expect(employee.getRole()).toBe('Employee');
+})
---
+++ __tests__/engineer.test.js
@@ -0,0 +1,23 @@
+const Engineer = require('../lib/Engineer');
+
+// this tests the parent class 
+
+// creates the new Engineer object 
+test('Creates new Engineer', () => {
+    const engineer = new Engineer('Alberto', 80, 'someEmail@gmail.com');
+
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
+    
+})
+
+// gets all the values from the methods 
+test('Checks all methods for Engineer class', () => {
+    const engineer = new Engineer('Alberto', 80, 'someEmail@gmail.com');
+
+    expect(engineer.getName()).toBe(engineer.name);
+    expect(engineer.getId()).toBe(engineer.id);
+    expect(engineer.getEmail()).toBe(engineer.email);
+    expect(engineer.getRole()).toBe('Engineer');
+})
